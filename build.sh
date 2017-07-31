356eaf05b7743385e2789c2a43399717ef6b68c3
Bruce Becker <brucellino@gmail.com>
2017-07-31 11:18:11 +0200

    how many  times I gotta say this ?!

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
 else
   echo "continuing from previous builds, using source at " ${SRC_DIR}/${SOURCE_FILE}
 fi
-tar xjf  ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE} --skip-old-files
+tar xzf  ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE} --skip-old-files
 mkdir -p ${WORKSPACE}/${NAME}${VERSION}/build-${BUILD_NUMBER}
 cd ${WORKSPACE}/${NAME}${VERSION}/build-${BUILD_NUMBER}
 
